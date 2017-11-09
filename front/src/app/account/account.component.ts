09b65ac6e21365f27ae98633e8a058eea8a80040
Léonard <picot.leonard@gmail.com>
2017-11-09 17:21:06 +0100

    Avancement du projet

---
+++ front/src/app/account/account.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from "../services/data.service";
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent implements OnInit {
+
+  sold:Number;
+  boolError:Boolean;
+  msgError:String;
+  boolSuccess:Boolean;
+  msgSuccess:String;
+  constructor(private dataService:DataService) {
+  }
+
+  ngOnInit() {
+    var result = this.dataService.getInfo();
+    result.subscribe(data => {
+      var body = JSON.parse(data['_body']);
+      if(body.type) {
+        this.sold = body.data.sold;
+      }
+    });
+  }
+
+  virement(montant,destinataire) {
+    this.boolSuccess = false;
+    this.msgSuccess= '';
+    this.boolError = false;
+    this.msgError = '';
+    montant = +montant;
+    if(isNaN(montant)) {
+      this.boolError = true;
+      this.msgError = "Veuillez saisir un montant"
+    }
+    if(destinataire === '') {
+      this.boolError = true;
+      this.msgError = this.msgError+" Veuillez saisir un destinataire"
+    }
+    if(this.boolError) {
+      return false;
+    }
+    var result = this.dataService.virement(montant,destinataire);
+    result.subscribe(data => {
+      var body = JSON.parse(data['_body']);
+      if(!body.type) {
+        this.boolError = true;
+        this.msgError = body.data;
+      } else {
+        this.boolSuccess = true;
+        this.msgSuccess = body.data;
+        return this.ngOnInit();
+      }
+    });
+  }
+}
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+
+import {DataService} from "./services/data.service";
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
